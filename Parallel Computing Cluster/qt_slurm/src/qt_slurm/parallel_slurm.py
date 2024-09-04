2e8d3f6d44759010129b01c5eb07d8cd7153f7e0
Dylan Kawashiri <87044258+dylankawashiri@users.noreply.github.com>
2024-09-04 11:35:49 -0700

    Update parallel_slurm.py

--- Parallel Computing Cluster/qt_slurm/src/qt_slurm/parallel_slurm.py
+++ Parallel Computing Cluster/qt_slurm/src/qt_slurm/parallel_slurm.py
@@ -11,8 +11,15 @@ import matplotlib.pyplot as plt
 import nbformat
 from nbconvert import PythonExporter
 
+'''
+Qt_Slurm v1.0.1
+'''
 
 def initialize():
+    """
+    The initialize() is for Jupyter notebooks. The function will set the loop condition so only one job is queued. 
+    Without this, the loop will be repeated when copied over to each node and then executed with srun. 
+    """
     home_dir = os.getenv('HOME')
     with open(home_dir + "/temporary_files/execute.csv", 'w') as f:
         write = csv.writer(f)
@@ -20,7 +27,12 @@ def initialize():
     
 def split_func(num_of_divs):
     '''
-    The split function specifies how many tasks each node will accomplish.
+    The split function specifies how many tasks each node will accomplish. It divides the total number of divisions 
+    by the total number of computers. This floor is added to an array with the number of entries set to the
+    number of computers. The remainder is calculated with a modulus and is added as evenly as possible to each
+    node. 
+
+    num_of_divs: Number of divisions or points in your param parameter for your main function. 
     '''
     rank, total_ranks = get_rank()
     
@@ -34,15 +46,26 @@ def split_func(num_of_divs):
 
     
 def get_rank():
+    '''
+    The get_rank() function gets the unique number assigned to each computer by Slurm (necessary for division) and the 
+    total number of computers available for the job. 
+    '''
     rank = int(os.getenv('SLURM_PROCID', 0))
     total_ranks = int(os.getenv('SLURM_NTASKS', 1))
     return rank, total_ranks
 
         
-def parallelize(func, detunings, num_of_divs):
+def parallelize(func, param, num_of_divs):
     '''
-    The Parallelize function uses Qutip's parallel_map feature and splits the number of tasks created by parallel_map and gives a specified amount to each node. 
-    Data is sent to a temporary file system and collected by the last node to complete its computation(s).
+    The Parallelize function uses Qutip's parallel_map feature and splits the number of tasks created by parallel_map and 
+    gives a specified amount to each node. Data is sent to a temporary file system and collected by the last node to 
+    complete its computation(s).
+    If used in Jupyter Notebook, it must be in the last cell with anything related to your computation (therefore 
+    must be in a different cell before the execute() function. 
+
+    func: Function you wish to parallelize using Qutip's parallel_map (see Qutip docs for more details).
+    param: Parameter for given func function.
+    num_of_divs: Number of divisions or points in your param parameter. 
     '''
     home_dir = os.getenv('HOME')
 
@@ -55,16 +78,16 @@ def parallelize(func, detunings, num_of_divs):
     results_arr = [[] for _ in range(total_ranks + 1)]
 
     
-    detunings_arr=[]
+    param_arr=[]
     for i in range(total_ranks):
         if i == 0:
-            detunings_arr.append(detunings[i:split_arr[i]])
+            param_arr.append(param[i:split_arr[i]])
         else:
-            detunings_arr.append(detunings[sum(split_arr[:i]):sum(split_arr[:i])+split_arr[i]])
+            param_arr.append(param[sum(split_arr[:i]):sum(split_arr[:i])+split_arr[i]])
 
     for i in range(total_ranks):
         if i == rank:
-            results = parallel_map(func,detunings_arr[i])
+            results = parallel_map(func,param_arr[i])
             results_array = np.array(results).tolist()
             
     try: #Makes a temporary folder
@@ -125,7 +148,7 @@ def parallelize(func, detunings, num_of_divs):
         results_arr = [i for j in results_arr for i in j]
         
         try:    
-            graph=plt.plot(detunings/(2*np.pi),results_arr)
+            graph=plt.plot(param/(2*np.pi),results_arr)
             sv = 0
             n = 0
             while(sv == 0):
@@ -153,8 +176,17 @@ def parallelize(func, detunings, num_of_divs):
     return results_arr
 
 def execute(name, nodes, cores, tasks):
+    '''
+    Jupyter Notebook-specific function. Copies the contents of the Jupyter Notebook and exports it to a Python file
+    in a shared filesystem at location $HOME/shared_scripts/. The function will then use Slurm to execute the job in
+    parallel using srun and the specified variables once the file has been uploaded. 
+
+    name: Name of the Jupyter Notebook (path optional if in $HOME directory) 
+    nodes: Number of nodes (computers) the user will request Slurm for
+    cores: Number of cores requested (max is the number specified in slurm.conf file - also number of cores per CPU)
+    tasks: Number of tasks requested (set to nodes, this will tell Slurm how many times to distribute the .py file)
+    '''
     #Parts from ChatGPT
-    
     home_dir = os.getenv('HOME')
     with open(home_dir + "/temporary_files/execute.csv", newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"',quoting=csv.QUOTE_MINIMAL)
@@ -181,10 +213,12 @@ def execute(name, nodes, cores, tasks):
                 print(f"Error: {e}")
 
         else:
-            ans = input("A file with the name of your Jupyter notebook file already exists, would you like to delete it and restart (will be done automatically)?")
-            if ans.lower().replace(" ", "") == "yes" or ans.lower().replace(" ", "") =="y":
-                os.remove(home_dir + "/shared_scripts/" + name.split(".")[0]+ ".py")
-                execute(name, nodes, cores, tasks)
-            else:
-                print("Exiting...")
-           
+            try:
+                ans = input("A file with the name of your Jupyter notebook file already exists, would you like to delete it and restart (will be done automatically)?")
+                if ans.lower().replace(" ", "") == "yes" or ans.lower().replace(" ", "") =="y":
+                    os.remove(home_dir + "/shared_scripts/" + name.split(".")[0]+ ".py")
+                    execute(name, nodes, cores, tasks)
+                else:
+                    print("Exiting...")
+            except Exception:
+                pass 
